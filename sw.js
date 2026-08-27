7bf095d9cfac7b522c5c6b854faf15ac20823ccd
Shaodreex <guilherme.sousawork@gmail.com>
2026-08-26 23:33:57 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='meu-financeiro-v12-darkmode';
+const CACHE='meu-financeiro-v13-iconsbrand';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./supabase-config.js','./manifest.webmanifest','./assets/icon.svg','./assets/icon-192.png','./assets/icon-512.png','./assets/apple-touch-icon.png'];
 
 self.addEventListener('install', event => {
